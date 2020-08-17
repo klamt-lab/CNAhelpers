1bc6bcc5e60eabb72505f81ef5a545b6e403ae31
Philipp Schneider <schneiderp@mpi-magdeburg.mpg.de>
2020-08-17 08:43:53 +0000

    normalized line endings

--- clr.m
+++ clr.m
@@ -1,3 +1,7 @@
+handles = double(findall(0,'type','figure'));
+delete(handles(~strcmp(get(handles,'Tag'),'CellNetAnalyzer')));
+cnan = rmfield(cnan,'open_projects');
+cnan.open_projects = struct;
 a=[setdiff(who,'cnan');{'a'}];
 clear(a{:});
 clc;
