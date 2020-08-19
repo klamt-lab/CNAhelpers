4ed37ad3006059e5c1c8d4821038026d6ace2213
Philipp Schneider <schneiderp@mpi-magdeburg.mpg.de>
2020-08-19 16:05:05 +0000

    Update clr.m

--- clr.m
+++ clr.m
@@ -1,5 +1,7 @@
 handles = double(findall(0,'type','figure'));
-delete(handles(~strcmp(get(handles,'Tag'),'CellNetAnalyzer')));
+if ~isempty(handles)
+    delete(handles(~strcmp(get(handles,'Tag'),'CellNetAnalyzer')));
+end
 cnan = rmfield(cnan,'open_projects');
 cnan.open_projects = struct;
 a=[setdiff(who,'cnan');{'a'}];
